a89a9a482e88677d1b938752e13d36f20f4254ec
kkehl-usgs <kkehl@contractor.usgs.gov>
2020-12-29 13:38:50 -0800

    IOW-796 fix chopping queue (#278)
    * fix chopping queue
    
    * code review feedback

--- trigger/tests/test_handler.py
+++ trigger/tests/test_handler.py
@@ -99,14 +99,19 @@ class TestLambdaHandler(TestCase):
         lambda_handler(self.sqs_event_two, self.context)
         self.assertEqual(mock_esm.call_count, 4)
 
-    @mock.patch.dict('os.environ', {'STATE_MACHINE_ARN': state_machine_arn, 'AWS_CHOPPING_QUEUE_URL': 'chopping',
+    @mock.patch.dict('os.environ', {'STATE_MACHINE_ARN': state_machine_arn, 'STAGE': 'TEST',
                                     'AWS_DEPLOYMENT_REGION': region,
                                     'OBJECT_SIZE_LIMIT': str(150000000)})
     @mock.patch('trigger.handler.execute_state_machine', autospec=True)
-    def test_giant_sqs_record(self, mock_esm):
-        mock_esm.return_value = {'spam': 'eggs', 'startDate': datetime.datetime(2020, 2, 18, 22, 00, 41)}
+    @mock.patch('trigger.handler.send_to_chopper', autospec=True)
+    def test_giant_sqs_record(self, mock_stc, mock_esm):
+        mock_stc.return_value = None
         lambda_handler(self.sqs_giant_event, self.context)
-        self.assertEqual(mock_esm.call_count, 4)
+        """
+        Zero calls because the giant file has been thrown in the chopping queue
+        """
+        mock_esm.assert_not_called()
+        mock_stc.assert_called_once()
 
     @mock.patch.dict('os.environ', {'STATE_MACHINE_ARN': state_machine_arn, 'AWS_DEPLOYMENT_REGION': region, 'OBJECT_SIZE_LIMIT': str(10**7)})
     @mock.patch('trigger.handler.execute_state_machine', autospec=True)
